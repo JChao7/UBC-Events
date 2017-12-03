eb4c844d3a29660659e41450a0c1ba57ba1ffa6c
[J_Chao7] <[jonathan.chao7@gmail.com]>
2017-12-02 16:39:26 -0800

    Comment Box

---
+++ Comment Box.js
@@ -0,0 +1,6 @@
+<script type="text/javascript" async>function ajaxpath_5a2346a403db5(url){return window.location.href == '' ? url : url.replace('&s=','&s=' + escape(window.location.href));}(function(){document.write('<div id="fcs_div_5a2346a403db5">
+<a title="free comment script" href="http://www.freecommentscript.com">&nbsp;&nbsp;<b>Free HTML User Comments</b>...</a>
+</div>');fcs_5a2346a403db5=document.createElement('script');fcs_5a2346a403db5.type="text/javascript";fcs_5a2346a403db5.src=ajaxpath_5a2346a403db5((document.location.protocol=="https:"?"https:":"http:")+"//www.freecommentscript.com/GetComments2.php?p=5a2346a403db5&s=#!5a2346a403db5");setTimeout("document.getElementById('fcs_div_5a2346a403db5').appendChild(fcs_5a2346a403db5)",1);})();</script><noscript><div>
+<a href="http://www.freecommentscript.com" title="free html user comment box">Free Comment Script</a>
+</div>
+</noscript>
